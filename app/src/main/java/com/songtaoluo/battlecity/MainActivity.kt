d1aad8a1d9db0c2f8a7f1d6fc3ad124149288703
songtaoluo007 <songtaoluo007@gmail.com>
2026-06-17 13:18:25 +0800

    feat: launch complete campaign flow from main activity

--- app/src/main/java/com/songtaoluo/battlecity/MainActivity.kt
+++ app/src/main/java/com/songtaoluo/battlecity/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
-import com.songtaoluo.battlecity.ui.BattleScreen
+import com.songtaoluo.battlecity.ui.BattleCityApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MaterialTheme {
-                BattleScreen()
+                BattleCityApp()
             }
         }
     }
