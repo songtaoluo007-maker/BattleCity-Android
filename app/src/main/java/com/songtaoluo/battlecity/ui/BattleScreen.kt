2cb37ba4c041b3ba84aca9e616c86d1e47289a92
songtaoluo007 <songtaoluo007@gmail.com>
2026-06-17 13:17:25 +0800

    refactor: inject configured engine into battle screen

--- app/src/main/java/com/songtaoluo/battlecity/ui/BattleScreen.kt
+++ app/src/main/java/com/songtaoluo/battlecity/ui/BattleScreen.kt
@@ -1,8 +1,12 @@
 package com.songtaoluo.battlecity.ui
 
 import androidx.compose.runtime.Composable
+import com.songtaoluo.battlecity.game.GameEngine
 
 @Composable
-fun BattleScreen() {
-    BattleContent()
+fun BattleScreen(
+    engine: GameEngine,
+    onExit: () -> Unit,
+) {
+    BattleContent(engine, onExit)
 }
